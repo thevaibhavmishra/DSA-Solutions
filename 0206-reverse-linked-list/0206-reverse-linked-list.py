388985bcda68e4a3a3bf3530180cf12e80e26abd
thevaibhavmishra <77563759+thevaibhavmishra@users.noreply.github.com>
2026-07-29 00:30:31 +0530

    Time: 0 ms (100%), Space: 20.5 MB (64.92%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,12 +5,13 @@
 #         self.next = next
 
 def revfun(head):
-    if not head or not head.next: return head, head
-    smallHead, tail = revfun(head.next)
+    if not head or not head.next: return head
+    smallHead = revfun(head.next)
+    tail = head.next
     tail.next = head
     head.next = None
-    return smallHead, head
+    return smallHead
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return revfun(head)[0]
+        return revfun(head)
