177c5099d6b8decc26142ae165143fea64fbd9d0
thevaibhavmishra <77563759+thevaibhavmishra@users.noreply.github.com>
2026-07-30 20:34:06 +0530

    Time: 8 ms (13.05%), Space: 19.3 MB (77.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,16 +4,6 @@
 #         self.val = val
 #         self.next = next
 
-def revFun(head):
-    if not head or not head.next: return head
-    prev = None
-    nex = None
-    while head:
-        nex = head.next
-        head.next = prev
-        prev = head
-        head = nex
-    return prev
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         ans = ListNode()
@@ -28,6 +18,7 @@ class Solution:
             ans = ans.next
             l1 = l1.next if l1 else l1
             l2 = l2.next if l2 else l2
+        print(tmp.next)
         if carry:
             ans.next = ListNode(1)
         return tmp.next
