45ac614d37b33c702120e3d03f2edec9ea88d875
thevaibhavmishra <77563759+thevaibhavmishra@users.noreply.github.com>
2026-08-02 18:02:42 +0530

    Time: 0 ms (100%), Space: 19.2 MB (71.97%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,12 +5,9 @@
 #         self.left = left
 #         self.right = right
 
-def check(p, q):
-    if not p and not q: return True
-    if not p or not q: return False
-    if p.val != q.val: return False
-    return check(p.left, q.left) and check(p.right, q.right)
-
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return check(p, q)
+        if not p and not q: return True
+        if not p or not q: return False
+        if p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
