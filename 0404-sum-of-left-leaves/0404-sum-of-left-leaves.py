d2ee75a6cbcff2155b6bd69d86cad0e2851fe9eb
thevaibhavmishra <77563759+thevaibhavmishra@users.noreply.github.com>
2026-08-07 18:16:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (92.37%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -5,13 +5,12 @@
 #         self.left = left
 #         self.right = right
 
-def findSum(root):
+def findSum(root, isLeft):
     if not root: return 0
-    s = 0
-    if root.left and not root.left.left and not root.left.right:
-        s += root.left .val
-    return findSum(root.left) + findSum(root.right) + s
+    if not root.left and not root.right:
+        return root.val if isLeft else 0
+    return findSum(root.left, True) + findSum(root.right, False)
 
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        return findSum(root)
+        return findSum(root, False)
